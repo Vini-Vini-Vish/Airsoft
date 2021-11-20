248bd134c7b9ce29796737756bc9513dac47666c
Vinícius Henrique do Nascimento <68130981+Vini-Vini-Vish@users.noreply.github.com>
2021-11-19 23:14:56 -0300

    Update pagina.js

--- src/inicio/pagina.js
+++ src/inicio/pagina.js
@@ -1,48 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import Esco from '../mural/escolhas'
 import {StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function PaginaPrincipal ({navigation }) {
+  return (
+    <View style = {styles.boxFora}>
+      <View style = {styles.container}>
+          
+          <View style = {styles.boxTitle}>
+
+            <Text style = {styles.textTitle}>Podendo alugar campos para realizar jogos fechados.</Text>
+            <Text style = {styles.textTitle}>App para compra ou aluguel de intens para jogos.</Text>
+
+          </View>           
+
+          <View>
+
+            <Text style = {styles.textSubTitle}>ESCOLHA A OPÇÃO DESEJADA:</Text>
+          
+          </View>
+
+          <View style = {styles.textCorpo}>
+
+            <Text style = {styles.textComunm}>O botão de Cadastros, contem as opções: Equipamentos, Campos, Usuario</Text>
+            <Button  
+              title = 'Cadastros'
+              onPress = {() => navigation.navigate('')}
+            ></Button>
+
+            <Text style = {styles.textComunm}>O botão de Mural, contem as opções: Armas e Munição, Equipamentos de Proteção, Campos.</Text>          
+            <Button 
+              title = 'Mural'
+              onPress = {() => navigation.navigate('')}
+            ></Button>
+
+            <Text style = {styles.textComunm}>O botão de Perfil, redireciona para a vizualização dos dados cadastrados.</Text>
+            <Button 
+              title = 'Perfil'
+              onPress = {() => navigation.navigate('')}
+            ></Button>
+              
+          </View>
+      </View>  
+    </View>  
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function Pagina() {
   return (
-    <View>
 
-      <View style = {styles.boxTitle}>
-        <Text style = {styles.textTitle}>Podendo alugar campos para realizar jogos fechados.</Text>
-        <Text style = {styles.textTitle}>App para compra ou aluguel de intens para jogos.</Text>
-      </View>           
-    
-        <View>
-            <Text style = {styles.textSubTitle}>ESCOLHA A OPÇÃO DESEJADA:</Text>
+    <NavigationContainer>
+      <Stack.Navigator>
 
-            <View style = {styles.textCorpo}>
-              
-              <Text style = {styles.textComunm}>O botão de Cadastros, contem as opções: Equipamentos, Campos, Usuario</Text>
-              <Button  
-                title = 'Cadastros'
-                onPress = {() => navigation.navigate('Esco')}
-              >                
-              </Button>
+        <Stack.Screen name = "Pagina" component = {PaginaPrincipal} />
 
-              <Text style = {styles.textComunm}>O botão de Mural, contem as opções: Armas e Munição, Equipamentos de Proteção, Campos.</Text>
-              <Button title = 'Mural'></Button>
-              
-              <Text style = {styles.textComunm}>O botão de Perfil, redireciona para a vizualização dos dados cadastrados.</Text>
-              <Button title = 'Perfil'></Button>
-                  
-            </View>
-        </View>    
-     
-    </View>
-  )
-};
+      </Stack.Navigator>
+    </NavigationContainer>
+
+  );
+}
 
 const styles = StyleSheet.create({
+  
+  boxFora: {
+    flex: 1,  
+    backgroundColor: '#000000', 
+  },
 
-   boxTitle: {
+  container: {
+    flex: 1,           
+    color: 'black',
+    borderRadius: 0,        
+    marginLeft: 5, 
+    marginTop: 10,
+    marginRight: 5,
+    marginBottom: 10,  
+    backgroundColor: '#F0F8FF',     
+  },
+  
+  boxTitle: {
     padding: 30,
-    backgroundColor: '#1e90ff',    
-    borderRadius: 30,   
+    backgroundColor: '#1e90ff',       
   },
 
   textTitle: {    
