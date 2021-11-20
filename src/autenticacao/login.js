396e2280e99ba5bf497e32e6bb2edc3b1d511a6b
Vinícius Henrique do Nascimento <68130981+Vini-Vini-Vish@users.noreply.github.com>
2021-11-20 16:14:41 -0300

    Create login.js

---
+++ src/autenticacao/login.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {Text, View, Button, TextInput, } from 'react-native';
+import styles from '../../style';
+
+export default function LoginUser ({navigation }) {
+    return (
+        <View style = {styles.boxFora}>       
+
+          <View style = {styles.container}>
+
+            <Text style = {styles.boxTitleBase}>REALIZE O LOGIN</Text>
+
+            <View style = {styles.textCorpo}>
+
+              <Text style = {styles.textSubTitleLogin}>Email e Senha Cadastrados</Text>
+              <TextInput 
+                style = {styles.textInput}
+                placeholder = 'nome@gmail.com'
+                //value={text} tem que criar a variavel
+              ></TextInput>                    
+              <TextInput 
+                style = {styles.textInput}
+                placeholder = '15afaf475'
+                keyboardType = 'numeric'
+              ></TextInput>
+
+              <View style = {styles.textButao}>
+                <Button  
+                  title = 'Logar'
+                  onPress = {() => navigation.navigate('Pagina')}
+                ></Button>
+              </View>                   
+
+              <View style = {styles.textCorpo}>
+                <Text style = {styles.textSubTitleLogin}>Cadastro de novo Usuario</Text>
+                <View style = {styles.textButao}>
+                  <Button                         
+                  title = 'Cadastro de Usuario'
+                  //onPress = {() => navigation.navigate('')}
+                  ></Button>  
+                </View>    
+              </View>                                                   
+
+            </View>  
+          </View>                               
+        </View>
+    )
+}
