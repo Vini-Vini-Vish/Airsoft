f1faede415cfe876ac51fa6725880bb027a858fa
Vinícius Henrique do Nascimento <68130981+Vini-Vini-Vish@users.noreply.github.com>
2021-11-20 00:08:06 -0300

    Update App.js

--- App.js
+++ App.js
@@ -3,10 +3,13 @@ import React from 'react';
 import Pagina from './src/inicio/pagina';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Login from './src/autenticacao/user';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Pagina/>
+      {/* <Pagina/> */}
+      <Login/>
     </View>
   );
 }
