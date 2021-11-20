cecce349cb506f6aa95d32d106bdc2b53526f2f1
Vinícius Henrique do Nascimento <68130981+Vini-Vini-Vish@users.noreply.github.com>
2021-11-20 16:14:48 -0300

    Update itens.js

---
+++ src/cadastros/itens.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View, Button } from 'react-native';
+import styles from '../../style';
+
+export default function EscolhasMU ({navigation }) {
+    return (
+       <View>
+           
+       </View>
+    )
+}
