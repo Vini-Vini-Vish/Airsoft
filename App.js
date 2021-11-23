e173b2d27547e09adfd8a4ff0be3009ad9d910ca
Vinícius Henrique do Nascimento <68130981+Vini-Vini-Vish@users.noreply.github.com>
2021-11-22 22:09:33 -0300

    Update App.js

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import LoginUser from './src/autenticacao/login';
 import ItensCad from '../airsoft/src/cadastros/itens'
 import LocalCad from '../airsoft/src/cadastros/local'
 import userCad from '../airsoft/src/cadastros/usuario'
+import perfilAlt from '../airsoft/src/usuario/perfil'
 
 const Stack = createNativeStackNavigator();
 
@@ -18,12 +19,14 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
 
-      {/*   <Stack.Screen name = "Logar" component = {LoginUser} />
+        <Stack.Screen name = "Logar" component = {LoginUser} />
         <Stack.Screen name = "Pagina" component = {PaginaPrincipal} />
         <Stack.Screen name = "EscolherMU" component = {EscolhasMU} />
-        <Stack.Screen name = "EscolherCA" component = {EscolhasCA} /> */}
+        <Stack.Screen name = "EscolherCA" component = {EscolhasCA} />
         <Stack.Screen name = "CadastrarIT" component = {ItensCad} />
-        <Stack.Screen name = "CadastrarUser" component = {LocalCad} />
+        <Stack.Screen name = "CadastrarLo" component = {LocalCad} />
+        <Stack.Screen name = "CadastrarUser" component = {userCad} />
+        <Stack.Screen name = "alterarUser" component = {perfilAlt} />
 
       </Stack.Navigator>
     </NavigationContainer>
