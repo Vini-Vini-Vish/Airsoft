772293c92d7c333aaebb3d8ca2e6bd9989c9ea12
Vinícius Henrique do Nascimento <68130981+Vini-Vini-Vish@users.noreply.github.com>
2021-11-22 21:56:24 -0300

    Update App.js

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import EscolhasCA from '../airsoft/src/cadastros/escoCA';
 import LoginUser from './src/autenticacao/login';
 import ItensCad from '../airsoft/src/cadastros/itens'
 import LocalCad from '../airsoft/src/cadastros/local'
+import userCad from '../airsoft/src/cadastros/usuario'
 
 const Stack = createNativeStackNavigator();
 
@@ -17,12 +18,12 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
 
-        <Stack.Screen name = "Logar" component = {LoginUser} />
+      {/*   <Stack.Screen name = "Logar" component = {LoginUser} />
         <Stack.Screen name = "Pagina" component = {PaginaPrincipal} />
         <Stack.Screen name = "EscolherMU" component = {EscolhasMU} />
-        <Stack.Screen name = "EscolherCA" component = {EscolhasCA} />
+        <Stack.Screen name = "EscolherCA" component = {EscolhasCA} /> */}
         <Stack.Screen name = "CadastrarIT" component = {ItensCad} />
-        <Stack.Screen name = "CadastrarLo" component = {LocalCad} />
+        <Stack.Screen name = "CadastrarUser" component = {LocalCad} />
 
       </Stack.Navigator>
     </NavigationContainer>
