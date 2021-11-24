5258a307674a55961476f964877321da91b4d799
Vinícius Henrique do Nascimento <68130981+Vini-Vini-Vish@users.noreply.github.com>
2021-11-24 20:50:27 -0300

    Update ar_mu.js

--- src/mural/ar_mu.js
+++ src/mural/ar_mu.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {StyleSheet, Text, View, Button } from 'react-native';
+import styles from '../../style';
 
 export default function Mural_ARMU() {
     return (
-      <View>       
-       
+      <View style = {styles.boxFora}>  
+        <View style = {styles.container}>
+          <View>
+            <Text style = {styles.boxTitleBase}>Mural de Armas e Munições</Text>     
+          </View>
+
+          <View>
+            <li>teste</li>
+          </View>     
+        </View>
       </View>
     )
   };
