db1a90dab04ac1c15ebbef01fab8aab39c69158b
Vinícius Henrique do Nascimento <68130981+Vini-Vini-Vish@users.noreply.github.com>
2021-11-26 17:18:27 -0300

    Create 20211126200922-create-cad-iten.js

---
+++ migrations/20211126200922-create-cad-iten.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('CadItens', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nomeIt: {
+        type: Sequelize.STRING
+      },
+      desIt: {
+        type: Sequelize.STRING
+      },
+      anoComIt: {
+        type: Sequelize.INTEGER
+      },
+      desAquiIt: {
+        type: Sequelize.STRING
+      },
+      desNacIt: {
+        type: Sequelize.STRING
+      },
+      condIt: {
+        type: Sequelize.STRING
+      },
+      precoIt: {
+        type: Sequelize.FLOAT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('CadItens');
+  }
+};
